d241098ef6c5b8cd687ece220788ac5d3f846ff1
Muhammad Safri Juliardi <juliardi@student.uns.ac.id>
2016-10-03 10:15:40 +0700

    fix missing variable

--- src/Helper/Log.php
+++ src/Helper/Log.php
@@ -9,6 +9,10 @@ class Log
 {
     public static function __callStatic($name, $arguments)
     {
+        $methodNames = array('emergency', 'alert', 'critical',
+                            'error', 'warning', 'notice',
+                            'info', 'debug', );
+
         if (in_array($name, $methodNames)) {
             $logger = app()->logger;
             call_user_func_array([$logger, $name], $arguments);
